f255da0cd29136e9da2f970c0e9ff37bfb314a8d
Nilp Amin <nilpamin2@gmail.com>
2021-08-15 23:11:22 +1000

    Initating repo.

---
+++ metr2800-robot/metr2800-robot/main.c
@@ -0,0 +1,20 @@
+/*
+ * metr2800-robot.c
+ *
+ * Created: 15/08/2021 11:00:45 PM
+ * Author : Nilp
+ */ 
+
+#define F_CPU	16000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) {
+    }
+}
+
