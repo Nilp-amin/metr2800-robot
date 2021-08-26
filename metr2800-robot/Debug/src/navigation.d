d9ff16b6ab5fd960c946eeb8142b383a877fd8be
algoth3214 <lucasladyman@gmail.com>
2021-08-27 02:09:41 +1000

    Added draft function to determining which side of the robot the centre of the square will be. Added function calls in navigation.c to motor.c to test sensor->actuator.

--- metr2800-robot/Debug/src/navigation.d
+++ metr2800-robot/Debug/src/navigation.d
@@ -17,7 +17,8 @@ src/navigation.d src/navigation.o: ../src/navigation.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../src/../utils/motor.h
 
 ../src/../utils/navigation.h:
 
@@ -56,3 +57,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../src/../utils/motor.h:
